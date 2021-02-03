93e93789b0edd345d5d92d735fcbf589a086c4d0
Matt Felton <matthew.felton@senecacollege.ca>
2021-02-03 13:50:31 -0500

    integrated tailwind css
    integrated tailwind css

---
+++ themes/honey-cakes/footer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package honeycakes
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'honey-cakes' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'honey-cakes' ), 'WordPress' );
+				
+				?>
+			</a>
+			<span class="sep"> <p class="text-purple-600 ...">hiiiiii</p>| </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'honey-cakes' ), 'honey-cakes', '<a href="https://mattfelton.com/">Matt Felton</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
